473c5b3a8b608152f438eb33d634c78c96af06fb
yusuf-madkour <yusuf.madkour@gmail.com>
2020-06-26 06:48:32 +0200

    Reformatted code

--- 1- ASCII art/src/asciiart.py
+++ 1- ASCII art/src/asciiart.py
@@ -22,19 +22,20 @@ if __name__ == "__main__":
     short_options = "f:c:im:"
     long_options = ["file=", "color=", "inv", "mode="]
     try:
-        arguments, _ = getopt.getopt(argument_list, short_options, long_options)
+        arguments, _ = getopt.getopt(
+            argument_list, short_options, long_options)
     except getopt.error as err:
         print(str(err))
         sys.exit(2)
 
     for current_argument, current_value in arguments:
         if current_argument in ("-f", "--file"):
-            image_path = current_value  # Image name, image has to be in the same directory
+            image_path = current_value  # Image name, image has to be in the same directory if full path was not passed
         elif current_argument in ("-c", "--color"):
             colors = {'white': Fore.WHITE, 'green': Fore.GREEN,
-                    'red': Fore.RED, 'blue': Fore.BLUE}
+                      'red': Fore.RED, 'blue': Fore.BLUE}
             color = colors.get(current_value.lower(),
-                            Fore.WHITE)  # Colorama's color
+                               Fore.WHITE)  # Colorama's color
         elif current_argument in ("-i", "--inv"):
             inv = True
         elif current_argument in ("-m", "--mode"):
@@ -45,7 +46,9 @@ if __name__ == "__main__":
     ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
     # Converting to avoid overflow warning when computing pixel brightness values
     pixel_array = np.uint16(imread(image_path))
-    pixel_array = resize(pixel_array, (320, 240), preserve_range=True, anti_aliasing=True)
+    # Double check that preserve_range and anti_aliasing actually do something
+    pixel_array = resize(pixel_array, (320, 240),
+                         preserve_range=True, anti_aliasing=True)
     print("Successfully loaded image!")
     print(f"Image size: {pixel_array.shape}")
     height, width = pixel_array.shape[0], pixel_array.shape[1]
@@ -55,9 +58,11 @@ if __name__ == "__main__":
     pixel_array = 255 - pixel_array if inv else pixel_array
 
     modes = {'average': lambda p: (p[0] + p[1] + p[2]) // 3,  # Taking average of RGB values
-            # Taking average of minimum and maximum RGB values
-            'lightness': lambda p: (sorted(p)[2] + sorted(p)[0]) / 2,
-            'luminosity': lambda p: 0.299 * p[0] + 0.587 * p[1] + 0.114 * p[2]}  # Multiplying each color with a weight
+             # Taking average of minimum and maximum RGB values
+             'lightness': lambda p: (sorted(p)[2] + sorted(p)[0]) / 2,
+             # Multiplying each color with a weight
+             'luminosity': lambda p: 0.299 * p[0] + 0.587 * p[1] + 0.114 * p[2]
+             }
 
     print("Converting to brightness matrix...")
 

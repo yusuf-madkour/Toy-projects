9a3646b4ae2c8a864be82aa39c24386e7360b342
yusuf-madkour <yusuf.madkour@gmail.com>
2020-06-30 09:47:51 +0200

    Script can now load patterns from text files

--- 2-Life/src/life.py
+++ 2-Life/src/life.py
@@ -1,15 +1,23 @@
 import argparse
-from random import choices, seed
+from random import choices
 from colorama import Fore, init
 from copy import deepcopy
 from time import sleep
 from os import system
 
-seed(42)
 
 def random_state(width, length):
     return [choices([0, 1], k=width, weights=[1-args.thresh, args.thresh]) for _ in range(length)]
 
+
+def load_board_state(filename):
+    try:
+        with open('../patterns/' + filename, mode='r') as f:
+            return [[int(c) for c in row.rstrip('\n')] for row in f.readlines()]
+    except:
+        print("File does not exist, generating a random pattern...")
+        sleep(5)
+        return random_state(args.width, args.length)
 
 def render(board):
     b = ''
@@ -18,29 +26,31 @@ def render(board):
         b += Fore.WHITE + '|'
         for c in row:
             if c == 1:
-                b += Fore.GREEN + '#' * args.scale
+                b += Fore.CYAN + pc * args.scale
             else:
-                b += Fore.RED + '#' * args.scale
+                b += Fore.MAGENTA + pc * args.scale
         b += Fore.WHITE + '|' + '\n'
     b += Fore.WHITE + '-' * (args.scale * len(board[0])+2) + '\n'
     print(b)
 
-def find_neighbours(brd):
-    ns = [[[] for cell in row] for row in brd]
-    for i, row in enumerate(brd):
+
+def moore_neighbours(board):
+    ns = [[[] for _ in row] for row in board]
+    for i, row in enumerate(board):
         for j, _ in enumerate(row):
             if i > 0:
-                ns[i][j].extend(brd[i-1][max(0, j-1):j+2])
-            if i < len(brd)-1:
-                ns[i][j].extend(brd[i+1][max(0, j-1):j+2])
+                ns[i][j].extend(board[i-1][max(0, j-1):j+2])
+            if i < len(board)-1:
+                ns[i][j].extend(board[i+1][max(0, j-1):j+2])
             if j > 0:
-                ns[i][j].append(brd[i][j-1])
-            if j < len(brd[i])-1:
-                ns[i][j].append(brd[i][j+1])
+                ns[i][j].append(board[i][j-1])
+            if j < len(board[i])-1:
+                ns[i][j].append(board[i][j+1])
     return ns
 
+
 def next_board_state(board):
-    n = find_neighbours(board)
+    n = moore_neighbours(board)
     new_board = deepcopy(board)
     for i, row in enumerate(board):
         for j, _ in enumerate(row):
@@ -52,13 +62,15 @@ def next_board_state(board):
                 new_board[i][j] = 1
     return new_board
 
+
 text = {
     'general': 'This is my guided implementation of "Conway\'s Game of Life", guided by Robert Heaton.',
     'scale': 'scales the width of the board by given multiplier, scale value must be of type integer',
     'width': 'specify width of the board, must be integer',
     'length': 'specify length of the board, must be integer',
     'sleep': 'specify sleep time after every board render, values are expected to be float (in seconds)',
-    'threshold': 'Higher threshold results in higher probability of alive cells in the board'}
+    'threshold': 'Higher threshold results in higher probability of alive cells in the board',
+    'pattern': 'pass the name of a text file that holds a 2d matrix of an initial state, file must be in patterns folder'}
 
 if __name__ == '__main__':
     init()  # Initializing colorama
@@ -72,22 +84,19 @@ if __name__ == '__main__':
     parser.add_argument(
         '-l', '--length', help=text['length'], type=int, default=30, metavar="")
     parser.add_argument(
-        '-sl', '--sleep', help=text['sleep'], type=float, default=0.3, metavar="")
+        '-sl', '--sleep', help=text['sleep'], type=float, default=0.01, metavar="")
     parser.add_argument(
         '-t', '--thresh', help=text['threshold'], type=float, default=0.5, metavar="")
+    parser.add_argument(
+        '-p', '--pattern', help=text['pattern'], type=str, metavar="")
     args = parser.parse_args()
 
     # Board initialization
-    b = random_state(args.width, args.length)
-    i = 0
-    while not (b == next_board_state(b) or b == next_board_state(next_board_state(b))):
+    b = load_board_state(args.pattern) if args.pattern else random_state(args.width, args.length)
+    pc = '%'
+    while b != next_board_state(b):
         render(b)
         sleep(args.sleep)
         b = next_board_state(b)
-        # system('cls')
-        i += 1
-        if i == 3: break
-    # render(next_board_state(b))
-    # system('cls')
-    # render(next_board_state(b))
-
+        system('cls')
+    render(next_board_state(b))

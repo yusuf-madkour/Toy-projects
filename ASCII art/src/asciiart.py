00fb2fcc44b1097419f6a23e34806345550e52cd
yusuf-madkour <yusuf.madkour@gmail.com>
2020-07-04 18:51:43 +0200

    replaced getopt with argparse

--- ASCII art/src/asciiart.py
+++ ASCII art/src/asciiart.py
@@ -1,40 +1,34 @@
 ## Imports ##
-import getopt
-import sys
+import argparse
 import numpy as np
 from colorama import Fore, init
 from skimage.io import imread
 from skimage.transform import resize
 
+text = {
+    'general': 'This is my guided implementation of ASCII image renderer, guided by Robert Heaton.',
+    'file': 'path to the image you want to render',
+    'mode': 'Choose brightness mode, available options are average, lightness and luminosity. Default is average.',
+    'inverted': 'Render the image inverted',
+    'color': 'Choose a color you want the image to be rendered in, default is green.'}
+
 if __name__ == "__main__":
-    ## Script parameters' defaults ##
-    inv = False
-    mode = 'average'
-    color = Fore.WHITE
 
     ## Arguments' handling ##
-    argument_list = sys.argv[1:]
-    short_options = "f:c:im:"
-    long_options = ["file=", "color=", "inv", "mode="]
-    try:
-        arguments, _ = getopt.getopt(
-            argument_list, short_options, long_options)
-    except getopt.error as err:
-        print(str(err))
-        sys.exit(2)
+    parser = argparse.ArgumentParser(description=text['general'])
+    parser.add_argument(
+        '-m', '--mode', help=text['mode'], type=str, default='luminosity', metavar="")
+    parser.add_argument('-i',
+                        '--inverted', help=text['inverted'], action='store_true')
+    parser.add_argument(
+        '-c', '--color', help=text['color'], type=str, default='white', metavar="")
+    parser.add_argument('file', help=text['file'], type=str, metavar="")
+    args = parser.parse_args()
 
-    for current_argument, current_value in arguments:
-        if current_argument in ("-f", "--file"):
-            image_path = current_value
-        elif current_argument in ("-c", "--color"):
-            colors = {'white': Fore.WHITE, 'green': Fore.GREEN,
-                      'red': Fore.RED, 'blue': Fore.BLUE}
-            color = colors.get(current_value.lower(),
-                               Fore.WHITE)  # Colorama's color
-        elif current_argument in ("-i", "--inv"):
-            inv = True
-        elif current_argument in ("-m", "--mode"):
-            mode = current_value.lower()
+    colors = {'white': Fore.WHITE, 'green': Fore.GREEN,
+              'red': Fore.RED, 'blue': Fore.BLUE}
+    color = colors.get(args.color.lower(),
+                       Fore.GREEN)  # Colorama's color
 
     ## Converting images' pixels to ASCII characters ##
 
@@ -42,11 +36,10 @@ if __name__ == "__main__":
     ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
     # Converting to avoid overflow warning when computing pixel brightness values
-    pixel_array = np.uint16(imread(image_path))
+    pixel_array = np.uint16(imread(args.file))
 
-    # TODO: Double check that preserve_range and anti_aliasing actually do something
-    pixel_array = resize(pixel_array, (320, 240),
-                         preserve_range=True, anti_aliasing=True)
+    # pixel_array = resize(pixel_array, (120, 240),
+    #                      preserve_range=True, anti_aliasing=True)
 
     print("Successfully loaded image!")
 
@@ -54,7 +47,7 @@ if __name__ == "__main__":
     init()  # Initializing colorama
 
     # Inverting colors if inv flag is passed to the script
-    pixel_array = 255 - pixel_array if inv else pixel_array
+    pixel_array = 255 - pixel_array if args.inverted else pixel_array
 
     modes = {'average': lambda p: (p[0] + p[1] + p[2]) // 3,  # Taking average of RGB values
              # Taking average of minimum and maximum RGB values
@@ -66,9 +59,10 @@ if __name__ == "__main__":
     print("Converting to brightness matrix...")
 
     # This nested list comprehension maps each pixel to the suitable ascii character based on the pixel's brightness and mode
-    converted_image = [[ascii[int(modes[mode](pixel) / 255 * 64)]
+    mode = modes.get(args.mode, modes['average'])
+    converted_image = [[ascii[int(mode(pixel) / 255 * 64)]
                         for pixel in row] for row in pixel_array]
-    
+
     ## Printing image to terminal ##
     print(f"printing image in {mode} mode")
     # Printing image, one row at a time

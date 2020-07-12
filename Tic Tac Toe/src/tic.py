aa09b493e9eacf9be0fb8eceeda8f0f81899408d
Yusuf <yusuf@pop-os.localdomain>
2020-07-13 01:20:20 +0200

    Fully working against random AI, colored render and consistent printouts

--- Tic Tac Toe/src/tic.py
+++ Tic Tac Toe/src/tic.py
@@ -1,32 +1,65 @@
 from random import choice
+from colorama import Fore, init
+from os import system
+import platform
+
+maps = {'1': (0, 0), '2': (0, 1), '3': (0, 2),
+        '4': (1, 0), '5': (1, 1), '6': (1, 2),
+        '7': (2, 0), '8': (2, 1), '9': (2, 2)}
+
 
 def new_board():
-    return [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    return [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+
+
+def get_lines(board):
+    return [
+        board[0], board[1], board[2],             # All rows
+        [board[0][0], board[1][0], board[2][0]],  # First column
+        [board[0][1], board[1][1], board[2][1]],  # Second column
+        [board[0][2], board[1][2], board[2][2]],  # Third column
+        [board[0][0], board[1][1], board[2][2]],  # First diagonal
+        [board[0][2], board[1][1], board[2][0]]   # Second diagonal
+    ]
 
 
 def render(board):
-    print('+-----+')
+    system('clear') if platform.system() == 'Linux' else system('cls')
+    print(cyan + '+-----+')
     for row in board:
-        print('|', end="")
-        print(*row, sep=' ', end='')
-        print('|')
-    print('+-----+')
+        print(cyan + '|', end='')
+        for i, c in enumerate(row):
+            if c == 'X':
+                print(red + 'X', end='')
+            elif c == 'O':
+                print(green + 'O', end='')
+            else:
+                print(c, end='')
+            if i != 2:
+                print(' ', end = "")
+        print(cyan + '|')
+    print(cyan + '+-----+')
 
 
 def get_move(board):
-    maps = {'1': (0, 0), '2': (0, 1), '3': (0, 2),
-            '4': (1, 0), '5': (1, 1), '6': (1, 2),
-            '7': (2, 0), '8': (2, 1), '9': (2, 2)}
-    render(board)
+    print(f"Player {player}, what is your move?")
     n = input()
-    while n not in list(maps.keys()):
+    while n not in maps:
+        render(board)
         print("Input must be a number between 1 and 9, please try again.")
+        print(f"Player {player}, what is your move?")
         n = input()
     return maps[n]
 
 
+def random_ai(board):
+    legal = [i for row in board for i in row if i not in ["X", "O"]]
+    return maps[choice(legal)]
+
+
 def make_move(board, coord, player):
     while board[coord[0]][coord[1]] in ['X', 'O']:
+        render(board)
         print('Cell already full, please try again.')
         coord = get_move(board)
     board[coord[0]][coord[1]] = player
@@ -38,14 +71,7 @@ def full_board(board):
 
 
 def get_winner(board):
-    lines = [
-             board[0], board[1], board[2],             # All rows
-             [board[0][0], board[1][0], board[2][0]],  # First column
-             [board[0][1], board[1][1], board[2][1]],  # Second column
-             [board[0][2], board[1][2], board[2][2]],  # Third column
-             [board[0][0], board[1][1], board[2][2]],  # First diagonal
-             [board[0][2], board[1][1], board[2][0]]   # Second diagonal
-             ]
+    lines = get_lines(board)
     for line in lines:
         if all(i == 'X' for i in line):
             return 'X'
@@ -55,18 +81,25 @@ def get_winner(board):
 
 
 if __name__ == "__main__":
-    player = choice(['O', 'X'])
+    init(autoreset=True)  # Initializing Colorama
+
+    # Colorama colors used in rendering the board
+    cyan = Fore.CYAN
+    red = Fore.RED
+    green = Fore.GREEN
+
+    player = choice(['O', 'X']) # Randomly choose who plays first
     board = new_board()
     while True:
-        print(f"Player {player}, what is your move?")
-        coords = get_move(board)
+        render(board)
+        coords = random_ai(board) if player == 'X' else get_move(board)
         board = make_move(board, coords, player)
         if get_winner(board):
-            print(f"Player {get_winner(board)} is the champion")
             render(board)
+            print(f"Player {get_winner(board)} wins")
             break
         if full_board(board):
-            print('We have a draw')
             render(board)
+            print('Draw!!')
             break
         player = 'O' if player == 'X' else 'X'
